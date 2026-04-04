c4f6b6d68ae53d5659bd81d07b2e577404de59cc
Carlos Soler García <wikicode96@gmail.com>
2026-04-04 19:14:38 +0200

    basic files

---
+++ addons/home_builder/HomeBuilderPlugin.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+[Tool]
+public partial class HomeBuilderPlugin : EditorPlugin
+{
+    public override void _EnterTree()
+    {
+        // Código de inicialización (opcional por ahora)
+    }
+
+    public override void _ExitTree()
+    {
+        // Código de limpieza (opcional por ahora)
+    }
+}
